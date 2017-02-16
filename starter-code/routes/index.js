85a5b2c99eca3f4a3f239fda2a9cb4edcf6dcfbd
Saturnino Villaverde <savianco@hotmail.com>
2017-02-16 22:41:55 +0100

    iteration 3 done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var Beach = require ('../models/beach');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+router.post('/beach', function(req, res, next) {
+  var nameB= req.body.beachName;
+  var flagB= req.body.flag;
+
+  console.log(req.body);
+  const beach =new Beach({beachName:nameB,flag:flagB});
+
+  Beach.findOne({"beachName": nameB},function(err,doc){
+    if (err) {
+      return next(err);
+      } else {
+        if(doc){
+          console.log("REPEATED");
+          Beach.findOneAndUpdate({beachName: nameB},{flag:flagB},function(err,doc){
+            if (err)
+              return next(err);
+          });////CHeck
+        } else{
+          beach.save((error) => {
+            if (error) {
+              return next(error);
+              } else {
+                  console.log(beach);
+                }
+            });
+        }
+     }
+  });
 });
 
 module.exports = router;
