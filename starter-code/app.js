cde1792f2b2595028b9d55ac53c832617f8ae898
Daniele <daniele.germani.85@gmail.com>
2017-07-13 15:56:19 +0200

    done iteration 1

--- starter-code/app.js
+++ starter-code/app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/beach-finder');
 
 var index = require('./routes/index');
 
