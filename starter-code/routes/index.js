87966f9df66c81c39feb4f5a6f4408f71282828b
Ana Milojevic <anamilojevicd@gmail.com>
2017-07-14 09:29:12 +0200

    wip

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var Beach = require ('../models/Beach');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/beach', function(req, res, next) {
+  var name = req.body.name;
+  var flag = req.body.flag;
+
+  const beach = new Beach({ name, flag });
+
+  Beach.findOne({name:name, function(err, i){
+    if(err){return next(err);
+    }else{
+      if(i){Beach.findOneAndUpdate({name: name}, {flag:flag}, function(err, i){
+        if(err){return next(err);
+        }else{
+          beach.save((e) => {
+            if(e) {return next(e);}
+          })
+        }
+      })}
+    }
+  }});
+
 });
 
 module.exports = router;
