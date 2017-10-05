96e61d203455ab6cfef38eff46eefdb7672b2850
Nicolas <nicolas.barthez@gmail.com>
2017-10-05 15:26:06 +0200

    done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  const place = "";
+  res.render("index", { place: place });
+});
+
+router.post("/", (req, res) => {
+  const place = req.body.search ? req.body.search : "";
+  console.log(place);
+  res.render("index", { place: place });
 });
 
 module.exports = router;
