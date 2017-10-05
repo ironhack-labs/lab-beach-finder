03ac6d3d093f06b2f3cad35b0bd84d9bcc34f699
Elo <wanang.elodie@hotmail.fr>
2017-10-05 15:33:29 +0200

    PAR Valentin/Elodie -- Iteration 1 + 2+3 almost done

---
+++ starter-code/public/javascripts/main.js
@@ -0,0 +1,75 @@
+$(document).ready(function(){
+  const sol = {
+    lat: 40.417080,
+    lng: -3.703612
+  };
+const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: sol
+  });
+
+  const input =(
+            document.getElementById('autocomplete'));
+
+  const autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.bindTo('bounds', map);
+
+// const infowindow = new google.maps.InfoWindow();
+// $('searchButton').click({
+//   autocomplete.getPlace();
+//
+//})
+var infowindow = new google.maps.InfoWindow();
+        var marker = new google.maps.Marker({
+          map: map,
+          anchorPoint: new google.maps.Point(0, -29)
+        });
+
+        autocomplete.addListener('place_changed', function() {
+          infowindow.close();
+          marker.setVisible(false);
+          var place = autocomplete.getPlace();
+          if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+          }
+          // If the place has a geometry, then present it on a map.
+           if (place.geometry.viewport) {
+             map.fitBounds(place.geometry.viewport);
+           } else {
+             map.setCenter(place.geometry.location);
+             map.setZoom(17);  // Why 17? Because it looks good.
+           }
+           marker.setIcon(/** @type {google.maps.Icon} */({
+             url: place.icon,
+             size: new google.maps.Size(71, 71),
+             origin: new google.maps.Point(0, 0),
+             anchor: new google.maps.Point(17, 34),
+             scaledSize: new google.maps.Size(35, 35)
+           }));
+           marker.setPosition(place.geometry.location);
+           marker.setVisible(true);
+
+           var address = '';
+           if (place.address_components) {
+             address = [
+               (place.address_components[0] && place.address_components[0].short_name || ''),
+               (place.address_components[1] && place.address_components[1].short_name || ''),
+               (place.address_components[2] && place.address_components[2].short_name || '')
+             ].join(' ');
+           }
+           infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+          infowindow.open(map, marker);
+        });
+
+
+$('.flag').hide();
+$('#searchButton').click(function() {
+$('.flag').show();
+}
+
+)
+
+});
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
-
+const Beach = require('../models/beach');
+const mongoose = require('mongoose');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/' , (res,req,next) => {
+  const newBeach = new Beach ({
+    name : req.body.name,
+    flag : req.body.flag
+  })
+  // router.findOne({ username }, "username", (err, beach) => {
+  //   if (beach !== null) {
+  //     res.render("passport/signup", { message: "The username already exists" });
+  //     return;
+  //   }
+  //
+  //   const salt = bcrypt.genSaltSync(bcryptSalt);
+  //   const hashPass = bcrypt.hashSync(password, salt);
+  //
+  //   newBeach.save((err) => {
+  //     if (err) { return next(err)}
+  //
+  //   });
+  // });
 });
 
 module.exports = router;
