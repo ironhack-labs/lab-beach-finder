b3666e44d261f1f08aece1fc96b5e6c65d5330cd
popelini <ccastillon92@gmail.com>
2017-11-16 17:44:30 +0100

    Iteration 3 finished

---
+++ starter-code/models/beach.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const beachSchema = new Schema({
+  name : { type: String },
+  flag : {type: String},
+});
+
+const Beach = mongoose.model('Beach', beachSchema);
+module.exports = Beach;
