e3bee54524901ab824b24239cf49930ce89a1067
xarliex <carlos.lorente@hotmail.com>
2018-02-08 19:17:37 +0100

    Iteración 2

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,17 +1,23 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const layouts = require("express-ejs-layouts");
 
-var index = require('./routes/index');
+const index = require('./routes/index');
 
-var app = express();
+mongoose.connect("mongodb://localhost/beaches");
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set("layout", "layouts/main");
+app.use(layouts);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
