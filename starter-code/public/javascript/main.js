22ecc31b51fdae0d40f6352c3b43fe31a0b2db6e
jesusdelrioc <jesusdelrioc@gmail.com>
2018-02-08 18:13:16 +0100

    mapa

--- starter-code/public/javascript/main.js
+++ starter-code/public/javascript/main.js
@@ -1,11 +1,19 @@
 function startMap(){
-    const location={lat:42.827586, lng:-9.105653
-
-    };
-    const map = new google.maps.Map(document.getElementById('map'),{
+    const location={lat:42.827586, lng:-9.105653};
+    const map = new google.maps.Map(document.getElementById('map'),
+    {
       zoom: 13,
       center:location
     });
     }
+    let defaultBounds = new google.maps.LatLngBounds(
+        new google.maps.LatLng(42.827533, -9.105644),
+    )
     
+let options= {
+    bounds:defaultBounds
+};
+
+let input= document.getElementById("pac-input")
+
     startMap();
