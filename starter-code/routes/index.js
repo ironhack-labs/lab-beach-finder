37b7f5aef5a377a9b71a064ac06422d9c936957f
ngoc0905 <lebichngoc090589@gmail.com>
2018-02-08 15:31:56 +0100

    done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+router.post((req, res, next) => {
+  console.log(req.body);
+  const newBeach = new Beach({
+    name: req.body.name,
+    flag: req.body.flag
+  });
 });
 
 module.exports = router;
