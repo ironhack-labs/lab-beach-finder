49b7403c56254273462c54d4e3a1c19c4fd3d5d6
Jeff Campigli <jfcampigli@gmail.com>
2018-02-08 15:30:16 +0100

    PAR Arthur MOSSA Jeff

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,31 +1,45 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const index = require("./routes/index");
+const app = express();
+var google = require("google-maps");
 
-var index = require('./routes/index');
+// main.js
+function startMap() {
+  var ironhackBCN = {
+    lat: 41.3977381,
+    lng: 2.190471916
+  };
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: ironhackBCN
+  });
+}
 
-var app = express();
-
+startMap();
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', index);
+map.setCenter(new google.maps.LatLng(48.8746, 2.338121));
+
+app.use("/", index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -34,11 +48,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
