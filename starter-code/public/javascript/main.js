c1682b2b82e366cacb10be757ff04ff5b41907e3
jesusdelrioc <jesusdelrioc@gmail.com>
2018-02-08 20:13:58 +0100

    changed

--- starter-code/public/javascript/main.js
+++ starter-code/public/javascript/main.js
@@ -1,12 +1,12 @@
-function startMap(){
-    const location={lat:42.827586, lng:-9.105653};
-    const map = new google.maps.Map(document.getElementById('map'),
-    {
-      zoom: 13,
-      center:location
-    });
+// function startMap(){
+//     const location={lat:42.827586, lng:-9.105653};
+//     const map = new google.maps.Map(document.getElementById('map'),
+//     {
+//       zoom: 13,
+//       center:location
+//     });
     
-let input= document.getElementById('pac-input');
-let autocomplete = new  google.maps.places.Autocomplete(input);
- };
-  startMap();
+// let input= document.getElementById('pac-input');
+// let autocomplete = new  google.maps.places.Autocomplete(input);
+//  };
+//   startMap();
