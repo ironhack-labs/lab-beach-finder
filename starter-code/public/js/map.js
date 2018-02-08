d5c82c5ef816bc392bea109b41716f20fb750a39
Alvaro Martinez <alvaromartinez@MacBook-Pro-de-Alvaro.local>
2018-02-08 19:46:56 +0100

    Casi Iteración 2

--- starter-code/public/js/map.js
+++ starter-code/public/js/map.js
@@ -1,13 +1,66 @@
+
 function startMap() {
-    var ironhackBCN = {
-        lat: 41.3977381,
-        lng: 2.190471916};
-    var map = new google.maps.Map(
-      document.getElementById('map'),
-      {
-        zoom: 15,
-        center: ironhackBCN
+  const ironhackBCN = {
+    lat: 41.3977381,
+    lng: 2.190471916
+  };
+  let mapa = new google.maps.Map(
+    document.getElementById('map'),
+    {
+      zoom: 15,
+      center: ironhackBCN
+    }
+  );
+  var input = document.getElementById('playa');
+
+  var searchBox = new google.maps.places.SearchBox(input,
+  );
+
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function () {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function (marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function (place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
       }
-    );
-  }
-  
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+}
